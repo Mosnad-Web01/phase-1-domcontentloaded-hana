85ea5e90d0ce9e3992574dc68e83a38660b59aae
Hana Alhemyari <hana.saif.1978@gmail.com>
2024-08-15 14:31:34 +0300

    Update index.js
    .

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// ابدأ بالإستماع لحدث DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function() {
+    // عند تحميل DOM بالكامل، استهدف عنصر الفقرة وقم بتغيير النص الخاص به
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+  });
+  
